cbdf65246ee67243dde872922cdd3ab254a9455c
JhonSalya <salyamanoemadi@yahoo.com>
2018-04-13 22:44:49 +0800

    new activity - confirm payment - confirm payment detail - sales
    edited activity
    - cart
    - checkout
    - event detail
    - register
    - unpaid detail
    - upload receipt

--- app/src/main/java/com/example/jhonsalya/evist/ConfirmPaymentActivity.java
+++ app/src/main/java/com/example/jhonsalya/evist/ConfirmPaymentActivity.java
@@ -3,11 +3,11 @@ package com.example.jhonsalya.evist;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class PaymentConfirmActivity extends AppCompatActivity {
+public class ConfirmPaymentActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_payment_confirm);
+        setContentView(R.layout.activity_confirm_payment);
     }
 }
