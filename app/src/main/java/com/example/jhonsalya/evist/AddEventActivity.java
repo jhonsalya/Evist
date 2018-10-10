5b917e72c78f81cb8d15496753c5377eaf92983e
JhonSalya <salyamanoemadi@yahoo.com>
2018-10-11 06:07:06 +0800

    add event activity and edit layout button for submit clicking Note : Not tested yet, still debugging

--- app/src/main/java/com/example/jhonsalya/evist/AddEventActivity.java
+++ app/src/main/java/com/example/jhonsalya/evist/AddEventActivity.java
@@ -1,13 +1,249 @@
 package com.example.jhonsalya.evist;
 
+import android.app.DatePickerDialog;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.Calendar;
 
 public class AddEventActivity extends AppCompatActivity {
+
+    private static final int GALLERY_REQUEST = 2;
+    private Uri uri = null;
+    private ImageButton imageButton;
+    private EditText editName;
+    private EditText editCategory;
+    private EditText editLocation;
+    private EditText editPrice;
+    private EditText editDesc;
+    private EditText editStartDate;
+    private EditText editFinishDate;
+    private EditText editStartTime;
+    private EditText editNoOfParticipant;
+    private EditText editTargetAge;
+    private EditText editOrganizer;
+    private EditText editContact;
+
+    //bank info
+    private EditText editBankAccount;
+    private EditText editAccountNumber;
+    private EditText editAccountOwner;
+
+    private StorageReference storageReference;
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabaseUsers;
+    private FirebaseUser mCurrentUser;
+
+    Calendar mCurrentDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_event);
+
+        editName = (EditText) findViewById(R.id.edit_event_name);
+        editCategory = (EditText) findViewById(R.id.edit_category);
+        editLocation  = (EditText) findViewById(R.id.edit_location);
+        editPrice = (EditText) findViewById(R.id.edit_event_price);
+        editDesc = (EditText) findViewById(R.id.edit_event_description);
+        editStartDate = (EditText) findViewById(R.id.edit_start_date);
+        editFinishDate = (EditText) findViewById(R.id.edit_finish_date);
+        editStartTime = (EditText) findViewById(R.id.edit_start_time);
+        editNoOfParticipant = (EditText) findViewById(R.id.edit_event_participant);
+        editTargetAge = (EditText) findViewById(R.id.edit_target_age);
+        editOrganizer = (EditText) findViewById(R.id.edit_event_organizer);
+        editContact = (EditText) findViewById(R.id.edit_contact);
+
+        //bank info
+        editBankAccount = (EditText) findViewById(R.id.edit_event_bank_account);
+        editAccountNumber = (EditText) findViewById(R.id.edit_event_bank_account_no);
+        editAccountOwner = (EditText) findViewById(R.id.edit_event_bank_account_owner);
+
+        storageReference = FirebaseStorage.getInstance().getReference();
+        databaseReference = database.getInstance().getReference().child("EventApp");
+
+        mAuth = FirebaseAuth.getInstance();
+        mCurrentUser = mAuth.getCurrentUser();
+        mDatabaseUsers = FirebaseDatabase.getInstance().getReference().child("Users").child(mCurrentUser.getUid());
+
+        //show calendar in start date text view event
+        editStartDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCurrentDate = Calendar.getInstance();
+                int year = mCurrentDate.get(Calendar.YEAR);
+                int month = mCurrentDate.get(Calendar.MONTH);
+                int day = mCurrentDate.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog mDatePicker = new DatePickerDialog(AddEventActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int selectedYear, int selectedMonth, int selectedDay) {
+                        editStartDate.setText(selectedDay+"/"+selectedMonth+"/"+selectedYear);
+                        mCurrentDate.set(selectedYear, selectedMonth, selectedDay);
+                    }
+                }, year, month, day);
+                mDatePicker.show();
+            }
+        });
+
+        //show calendar in start date text view event
+        editFinishDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCurrentDate = Calendar.getInstance();
+                int year = mCurrentDate.get(Calendar.YEAR);
+                int month = mCurrentDate.get(Calendar.MONTH);
+                int day = mCurrentDate.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog mDatePicker = new DatePickerDialog(AddEventActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int selectedYear, int selectedMonth, int selectedDay) {
+                        editFinishDate.setText(selectedDay+"/"+selectedMonth+"/"+selectedYear);
+                        mCurrentDate.set(selectedYear, selectedMonth, selectedDay);
+                    }
+                }, year, month, day);
+                mDatePicker.show();
+            }
+        });
+    }
+
+    public void imageClicked(View view){
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent,GALLERY_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == GALLERY_REQUEST && resultCode == RESULT_OK){
+            uri = data.getData();
+            imageButton = (ImageButton) findViewById(R.id.addEventImage);
+            imageButton.setImageURI(uri);
+        }
+    }
+
+    private void changeImage(){
+        StorageReference filePath = storageReference.child("PostEvent").child(uri.getLastPathSegment());
+        filePath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                final Uri downloadurl = taskSnapshot.getDownloadUrl();
+                final DatabaseReference newPost = databaseReference.push();
+                newPost.child("image").setValue(downloadurl.toString());
+
+            }
+        });
+    }
+
+    public void addButtonClicked(View view){
+        final ProgressDialog mDialog = new ProgressDialog(AddEventActivity.this);
+        mDialog.setMessage("Please Wait.....");
+        mDialog.show();
+
+        if(uri != null){
+            changeImage();
+        }
+
+        final String nameValue = editName.getText().toString().trim();
+        final String categoryValue = editCategory.getText().toString().trim();
+        final String locationValue = editLocation.getText().toString().trim();
+        final String priceValue = editPrice.getText().toString().trim();
+        final String descValue = editDesc.getText().toString().trim();
+        final String startDateValue = editStartDate.getText().toString().trim();
+        final String finishDateValue = editFinishDate.getText().toString().trim();
+        final String startTimeValue = editStartTime.getText().toString().trim();
+        final String participantValue = editNoOfParticipant.getText().toString().trim();
+        final String targetAgeValue = editTargetAge.getText().toString().trim();
+        final String organizerValue = editOrganizer.getText().toString().trim();
+        final String contactValue =  editContact.getText().toString().trim();
+
+        final String bankAccountValue = editBankAccount.getText().toString().trim();
+        final String accountNumberValue = editAccountNumber.getText().toString().trim();
+        final String accountOwnerValue = editAccountOwner.getText().toString().trim();
+
+        if(!TextUtils.isEmpty(nameValue) &&
+                !TextUtils.isEmpty(categoryValue) &&
+                !TextUtils.isEmpty(locationValue) &&
+                !TextUtils.isEmpty(priceValue) &&
+                !TextUtils.isEmpty(descValue) &&
+                !TextUtils.isEmpty(startDateValue) &&
+                !TextUtils.isEmpty(finishDateValue) &&
+                !TextUtils.isEmpty(startTimeValue) &&
+                !TextUtils.isEmpty(participantValue) &&
+                !TextUtils.isEmpty(targetAgeValue) &&
+                !TextUtils.isEmpty(organizerValue) &&
+                !TextUtils.isEmpty(contactValue) &&
+                !TextUtils.isEmpty(bankAccountValue) &&
+                !TextUtils.isEmpty(accountNumberValue) &&
+                !TextUtils.isEmpty(accountOwnerValue)){
+
+            final DatabaseReference newPost = databaseReference.push();
+
+            mDatabaseUsers.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    newPost.child("title").setValue(nameValue);
+                    newPost.child("category").setValue(locationValue);
+                    newPost.child("location").setValue(participantValue);
+                    newPost.child("price").setValue(participantValue);
+                    newPost.child("description").setValue(participantValue);
+                    newPost.child("start_date").setValue(startDateValue);
+                    newPost.child("finish_date").setValue(finishDateValue);
+                    newPost.child("start_time").setValue(participantValue);
+                    newPost.child("participant").setValue(participantValue);
+                    newPost.child("target_age").setValue(participantValue);
+                    newPost.child("organizer").setValue(participantValue);
+                    newPost.child("contact").setValue(participantValue);
+                    newPost.child("bank_account").setValue(categoryValue);
+                    newPost.child("account_number").setValue(descValue);
+                    newPost.child("account_owner").setValue(mCurrentUser.getUid()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()){
+                                mDialog.dismiss();
+                                Toast.makeText(AddEventActivity.this, "Upload Complete", Toast.LENGTH_SHORT).show();
+                                Intent mainActivityIntent = new Intent(AddEventActivity.this, MainActivity.class);
+                                mainActivityIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                startActivity(mainActivityIntent);
+                            }
+                        }
+                    });
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+        }
     }
 }
